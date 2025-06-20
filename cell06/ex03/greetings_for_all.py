fb2cda293982d7e12b1ea3c9a9ce0e61210480e3
natabum <natarain2561@gmail.com>
2025-06-20 06:20:27 +0000

    6.ex03

---
+++ cell06/ex03/greetings_for_all.py
@@ -0,0 +1,12 @@
+def greetings(name="noble stranger"):
+    if not isinstance(name, str):
+        print("Error: The name must be a string.")
+    else:
+        print(f"Welcome, {name}!")
+
+
+if __name__ == "__main__":
+    greetings()            
+    greetings("Alice")              
+    greetings(123)
+    
